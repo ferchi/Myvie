2e5083a42499f52015ea96c259af85ec8a6512c1
ferchi <fsalinas628@gmail.com>
2021-10-31 20:45:59 -0600

    Se agregó el uso de firebase authentication para el registro e inicio de sesión de los usuarios

--- app/src/main/java/com/jfsb/myvie/first/LoginActivity.kt
+++ app/src/main/java/com/jfsb/myvie/first/LoginActivity.kt
@@ -3,34 +3,68 @@ package com.jfsb.myvie.first
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.jfsb.myvie.R
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.jfsb.myvie.databinding.ActivityLoginBinding
 import com.jfsb.myvie.main.MainActivity
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    private lateinit var mAuth: FirebaseAuth
+
+    private lateinit var txtEmail:String
+    private lateinit var txtPassword:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        mAuth = FirebaseAuth.getInstance()
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+
+        if(mAuth.currentUser!=null){
+            startApp()
+        }
+
         binding.tvSignUp.setOnClickListener {
-            var intent = Intent(this,RegisterActivity::class.java)
+            val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
         }
 
         binding.tvSignUpSecond.setOnClickListener {
-            var intent = Intent(this,RegisterActivity::class.java)
+            val intent = Intent(this,RegisterActivity::class.java)
             startActivity(intent)
         }
 
         binding.btnLogin.setOnClickListener{
-            var intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            this.finish()
+            txtEmail = binding.etLoginEmail.toString()
+            txtPassword = binding.etLoginPassword.text.toString()
+
+            if (txtEmail.isNotEmpty() && txtPassword.isNotEmpty()){
+                logUser()
+            }
+            else{
+                Toast.makeText(this,"Favor de ingresar todos los datos", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun logUser(){
+        mAuth.signInWithEmailAndPassword(txtEmail,txtPassword).addOnCompleteListener(){ task ->
+            if(task.isSuccessful){
+                startApp()
+                this.finish()
+            } else{
+                Toast.makeText(this,"Verificar los datos", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startApp(){
+        val intentProfile = Intent(this, MainActivity::class.java).apply {}
+        startActivity(intentProfile)
+        // Utils.openProfile(FirebaseDatabase.getInstance().reference, "Users", mAuth.currentUser!!.uid, requireActivity(), MainActivity(), true)
     }
 }
--- app/src/main/java/com/jfsb/myvie/first/RegisterActivity.kt
+++ app/src/main/java/com/jfsb/myvie/first/RegisterActivity.kt
@@ -1,13 +1,17 @@
 package com.jfsb.myvie.first
 
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.jfsb.myvie.databinding.ActivityRegisterBinding
+import com.jfsb.myvie.main.MainActivity
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegisterBinding
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,9 +19,58 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
         binding.tvLogin.setOnClickListener {
             this.finish()
         }
+
+        binding.btnSignup.setOnClickListener {
+            val db = FirebaseFirestore.getInstance()
+
+            if(binding.etRegisterUsername.text.isNotEmpty() && binding.etRegisterEmail.text.isNotEmpty() && binding.etRegisterPassword.text.isNotEmpty() && binding.etRegisterRepassword.text.isNotEmpty() ){
+                    if(binding.etRegisterPassword.text.toString() == binding.etRegisterRepassword.text.toString()){
+                            val mAuth = FirebaseAuth.getInstance()
+
+                            mAuth.createUserWithEmailAndPassword(binding.etRegisterEmail.text.toString(), binding.etRegisterPassword.text.toString()).addOnCompleteListener {
+
+                                if (it.isSuccessful) {
+                                    db.collection("Users").document(binding.etRegisterEmail.text.toString()).set(
+                                        hashMapOf (
+                                            "email" to binding.etRegisterEmail.text.toString(),
+                                            "name" to binding.etRegisterUsername.text.toString(),
+                                            "imgProfile" to ""
+                                        )
+                                    )
+                                    login(it.result?.user?.email ?:"", binding.etRegisterPassword.text.toString())
+                                }
+                            }
+                    }
+                    else{
+                        showAlert("Las contraseñas no coinciden")
+                    }
+            }
+            else{
+                showAlert("Favor de rellenar todos los campos")
+            }
+        }
+    }
+
+    private fun showAlert( message:String){
+        val toast = Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT)
+        toast.show()
+    }
+
+    private fun login(email: String, password: String){
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password).addOnCompleteListener {
+            if(it.isSuccessful)
+                showHome(it.result?.user?.email ?:"")
+            else
+                showAlert("Correo o contraseña incorrecto")
+        }
+    }
+    private fun showHome(email: String){
+        val homeIntent = Intent(this, MainActivity::class.java).apply {
+            putExtra("email", email)
+        }
+        startActivity(homeIntent)
     }
 }
